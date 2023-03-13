8140afa4dea7df65465fc3135d48f79e10b3ae11
Sebaswo <sebastwo@metropolia.fi>
2023-03-13 17:23:02 +0200

    test

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express()
 // middleware
 app.use(express.json())
 app.use(cors())
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 4000;
 
 
 app.use((req, res, next) => {
